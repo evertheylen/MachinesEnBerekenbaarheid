3b6fe79122ce3a826dce09b16ce43ec9a2812964
Evert Heylen <evertheylen@gmail.com>
2015-10-18 20:37:25 +0200

    coming soon, PDA

---
+++ code/MBLib/PDA/PDA.hpp
@@ -0,0 +1,23 @@
+
+/* PDA Library
+
+[bake me]
+
+
+[stop baking] */
+
+#ifndef H_PDA
+#define H_PDA
+
+class PDA {
+public:
+	// PDA P = (Q, Sigma, Gamma, delta, q_0, Z_0, F)
+	
+	int num_states;
+	
+};
+
+
+
+
+#endif
