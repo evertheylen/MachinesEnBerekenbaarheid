b9a6b739466251a1b061bd53d4f8c9ec8f1235e5
babbel056 <hermans.anthony@hotmail.com>
2015-11-29 20:39:32 +0100

    Load is almost done

--- code/NGLib/generator/generator.cpp
+++ code/NGLib/generator/generator.cpp
@@ -5,12 +5,23 @@
 #include "NGLib/replacor/replacor.hpp"
 #include "libs/tinyxml/tinyxml.h"
 #include <fstream>
+#include <string>
 #include <iostream>
+#include <exception>
 
+class exceptionXML: public std::exception{
+	public:
+	  virtual std::string syntacticError() const throw(){
+	    return "The inputXML contains syntactic errors\n";
+	  }
+		virtual std::string semanticError(std::string fault, std::string correct) const throw(){
+		 	std::string error = "The inputXML contains syntactic errors: " + fault + " instead of " + correct + ".\n";
+			return error;
+	  }
+};
 
 Generator::Generator(Outputter* _out, Replacor* _repl):
 	out(_out), repl(_repl) {}
-
 
 // if max_repl == -1 ==> Infinite (default)
 void Generator::generate(std::vector<std::string> start, int max_repl) {
@@ -24,23 +35,59 @@ void Generator::generate(std::vector<std::string> start, int max_repl) {
 
 // this function will load a specified XML file
 void Generator::loadXML(std::string inputfile){
+	exceptionXML myex;
 	if (inputfile == ""){
 		std::cout << "There is no file specified.\n";
 		return;
 	}
 	TiXmlDocument file;
 	file.LoadFile(inputfile);
-	//inputXML.LoadFile(loadfile);
-	// std::ifstream myfile (loadfile);
-	// std::string line;
-	// if (myfile.is_open()){
-	// 	while ( getline (myfile,line) ){
-	// 		std::cout << line << '\n';
-	// 	}
-	// 	myfile.close();
-	// }else{
-	// 	std::cout << "The specified XML file isn't valid\n";
-	// }
+	TiXmlElement* root = file.FirstChildElement();
+	if (root == NULL or root->FirstChildElement() == NULL){
+			std::cerr << "Exception caught: " << myex.syntacticError() << std::endl;
+			return;
+	}
+	std::string rootValue = root->Value();
+	if(rootValue.compare("Generator") == 0){
+		for( TiXmlElement* nextchild = root->FirstChildElement(); nextchild!= NULL; nextchild = nextchild->NextSiblingElement()){
+			std::string nextchildValue = nextchild->Value();
+			if(nextchildValue.compare("Outputter") == 0 or nextchildValue.compare("Replacor") == 0 or nextchildValue.compare("extra_setting") == 0){
+				if(nextchildValue.compare("extra_setting") == 0){
+					//Do something with value here
+				}else{
+					if(nextchildValue.compare("Outputter") == 0  and nextchild->Attribute("type") != NULL){
+						std::string nextchildAttribute = nextchild->Attribute("type");
+						if(nextchildAttribute.compare("PythonOutputter") == 0){
+							//Process input through PythonOutputter
+							continue;
+						}else{
+							std::cerr << "Exception caught: " << myex.semanticError(nextchildAttribute , "PythonOutputter") << std::endl;
+							return;
+						}
+					}
+					if(nextchildValue.compare("Replacor") == 0  and nextchild->Attribute("type") != NULL){
+						std::string nextchildAttribute = nextchild->Attribute("type");
+						if(nextchildAttribute.compare("CfgReplacor") == 0){
+							//Process input through CfgReplacor
+							continue;
+						}else{
+							std::cerr << "Exception caught: " << myex.semanticError(nextchildAttribute , "CfgReplacor") << std::endl;
+							return;
+						}
+					}else{
+						std::cerr << "Exception caught: " << myex.syntacticError() << std::endl;
+						return;
+					}
+				}
+			}else{
+				std::cerr << "Exception caught: " << myex.semanticError(nextchildValue , "Outputter/Replacor/extra_setting") << std::endl;
+				return;
+			}
+		}
+	}else{
+		std::cerr << "Exception caught: " << myex.semanticError(rootValue , "Generator") << std::endl;
+		return;
+	}
 }
 
 // this function will save to an XML file
@@ -58,8 +105,8 @@ void Generator::saveXML(std::string savefile){
 		output<< "		Information about CfgReplacor here\n";
 		output << "	</Replacor>\n";
 
-		output << "	<extra_setting>";
-		//Insert a randow value for extra setting here TODO
+		output << "	<extra_setting>\n";
+		output<< "		Information about extra settings here\n";
 		output << "	</extra_setting>\n";
 
 		output<<"</Generator>";
