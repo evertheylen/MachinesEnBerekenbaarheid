c6bcdf6b3c9c26268e20f11a9a4441c40755ca9d
Evert Heylen <evertheylen@gmail.com>
2015-12-05 17:17:20 +0100

    exceptions

--- code/NGLib/exceptions/exceptions.hpp
+++ code/NGLib/exceptions/exceptions.hpp
@@ -5,22 +5,25 @@
 class NGException: public std::exception {};
 
 class syntacticError: public NGException {
-	virtual const char* what() const {
+public:
+	virtual const char* what() const throw() {
 		return "The input XML contains syntactic errors\n";
 	}
 };
 
 class noValidFilename: public NGException {
-	virtual const char* what() const {
+public:
+	virtual const char* what() const throw() {
 		return "The file is not valid or not found\n";
 	}
 };
 
 class semanticError: public NGException {
+public:
 	semanticError(const std::string& _fault, const std::string& _correct):
 		fault(_fault), correct(_correct) {}
 	
-	virtual const char* what() const {
+	virtual const char* what() const throw() {
 		std::string error = "The inputXML contains semantic errors: " + fault + " instead of " + correct + ".\n";
 		return error.c_str();
 	}
