68b53a4ccf2c1d0294871090219132ad54f0af38
Stijn Janssens <janssens.stijn@hotmail.com>
2015-12-05 17:26:00 +0100

    replacor from Xml

--- code/NGLib/replacor/stochastic_replacor.hpp
+++ code/NGLib/replacor/stochastic_replacor.hpp
@@ -19,13 +19,7 @@ public:
 	using Rule_Type = StochasticRule<std::string>;
 	
 	StochasticReplacor(){};
-	StochasticReplacor(std::string cfg_file, std::random_device::result_type seed): mt(seed) {
-		TiXmlDocument doc;
-		if (!doc.LoadFile(cfg_file)) {
-			throw std::invalid_argument("Not existing filepath: " + cfg_file);
-		}
-		cfg(doc);
-	}
+	StochasticReplacor(TiXmlElement* elem, std::random_device::result_type seed): mt(seed), cfg(elem) {}
 	
 	Rule_Type replace(std::string var, std::list<SimpleRule<std::string>>& context) {
 		std::uniform_int_distribution<int> dist(0, 99);
