db5f51625ff1324d85ded509ac81f87931bfd068
Evert Heylen <evertheylen@gmail.com>
2015-12-05 17:03:10 +0100

    Mac support met prints

--- code/Core/bake_me.py
+++ code/Core/bake_me.py
@@ -11,6 +11,7 @@ dependencies["build_exec"] = [
 ]
 
 if platform.system() != 'Darwin':
+    print("I'm not a Mac, yay!")
     dependencies["build_exec"] += [
         "libs/boost>make>build",
         "libs/python3>make>all"
@@ -22,5 +23,8 @@ if platform.system() != 'Darwin':
         "pre_extra": "-Wno-deprecated -Ilibs/python3/Include -Ilibs/boost/b2stuff/include/",
         "post_extra": "-pthread -Wl,-Bstatic -Llibs/python3 -lpython3.4m -Llibs/boost/b2stuff/lib/ -lboost_python3 -Wl,-Bdynamic -lutil -ldl -lm"
     }
-
+    
+else:
+    print("I'm a Mac :(")
+    
 executable = "core"
