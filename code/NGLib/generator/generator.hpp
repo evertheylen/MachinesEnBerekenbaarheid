ac97ce20d5d615a1c16525078ec27628da1b09fe
babbel056 <hermans.anthony@hotmail.com>
2015-12-05 18:24:34 +0100

    update xml load and save

--- code/NGLib/generator/generator.hpp
+++ code/NGLib/generator/generator.hpp
@@ -18,15 +18,17 @@ dependencies["headers"] = [
 #include <vector>
 #include <string>
 #include <iostream>
-
 #include <exception>
 #include <stdexcept>
+#include <random>
 
 #include "libs/tinyxml/tinyxml.h"
 
 #include "MBLib/new_CFG/CFG.hpp"
 #include "NGLib/outputter/outputter.hpp"
+#include "NGLib/python_out/python_outputter.hpp"
 #include "NGLib/replacor/replacor.hpp"
+#include "NGLib/replacor/normal_replacor.hpp"
 #include "NGLib/exceptions/exceptions.hpp"
 
 
@@ -54,6 +56,7 @@ public:
 		if (!inputfile.compare("")) {
 			throw noValidFilename();
 		}
+		std::random_device rd;
 		TiXmlDocument file;
 		file.LoadFile(inputfile);
 		TiXmlElement* root = file.FirstChildElement();
@@ -61,40 +64,27 @@ public:
 			throw syntacticError();
 		}
 		std::string rootValue = root->Value();
-		if (rootValue.compare("Generator") == 0) {
+		if (rootValue.compare("GENERATOR") == 0) {
 			for (TiXmlElement* nextchild = root->FirstChildElement(); nextchild != NULL; nextchild = nextchild->NextSiblingElement()) {
-				std::string nextchildValue = nextchild->Value();
-				if (nextchildValue.compare("Outputter") == 0 or nextchildValue.compare("Replacor") == 0 or nextchildValue.compare("extra_setting") == 0) {
-					if (nextchildValue.compare("extra_setting") == 0) {
-						//Do something with value here
+				std::string nextchildValue = nextchild->GetText();
+				if (nextchildValue.compare("PYTHON_OUTPUTTER") == 0 or nextchildValue.compare("NORMAL_REPLACOR") == 0) {
+					if (nextchildValue.compare("PYTHON_OUTPUTTER") == 0) {
+						out = PythonOutputter("output1");
+						out->init();
+						continue;
+					}
+					if (nextchildValue.compare("NORMAL_REPLACOR") == 0) {
+						repl = NormalReplacor(nextchild, rd());
+						continue;
 					} else {
-						if (nextchildValue.compare("Outputter") == 0  and nextchild->Attribute("type") != NULL) {
-							std::string nextchildAttribute = nextchild->Attribute("type");
-							if (nextchildAttribute.compare("PythonOutputter") == 0) {
-								//Process input through PythonOutputter
-								continue;
-							} else {
-								throw semanticError(nextchildAttribute, "PythonOutputter");
-							}
-						}
-						if (nextchildValue.compare("Replacor") == 0  and nextchild->Attribute("type") != NULL) {
-							std::string nextchildAttribute = nextchild->Attribute("type");
-							if (nextchildAttribute.compare("CfgReplacor") == 0) {
-								//Process input through CfgReplacor
-								continue;
-							} else {
-								throw semanticError(nextchildAttribute , "CfgReplacor");
-							}
-						} else {
-							throw syntacticError();
-						}
+						throw syntacticError();
 					}
 				} else {
-					throw semanticError(nextchildValue , "Outputter/Replacor/extra_setting");
+					throw semanticError(nextchildValue , "Outputter/Replacor");
 				}
 			}
 		} else {
-			throw semanticError(rootValue , "Generator");
+			throw semanticError(rootValue , "GENERATOR");
 		}
 	}
 
@@ -105,24 +95,15 @@ public:
 		}
 		TiXmlDocument doc;
 		TiXmlDeclaration* decl = new TiXmlDeclaration("1.0", "", "");
-		TiXmlElement* root = new TiXmlElement("Generator");
-		
-		//Outputter
-		TiXmlElement* outputter = new TiXmlElement("Outputter");
-		outputter->SetAttribute("type", "PythonOutputter");
-			//Information about PythonOutputter here
-		root->LinkEndChild(outputter);
+		TiXmlElement* root = new TiXmlElement("GENERATOR");
 		
 		//CFG replacor
-		TiXmlElement* replacor = new TiXmlElement("Replacor");
-		replacor->SetAttribute("type", "CfgReplacor");
-			//Information about CfgReplacor here
+		TiXmlElement* replacor = repl->to_xml();
 		root->LinkEndChild(replacor);
 		
-		//Extra setting
-		TiXmlElement* extra_setting = new TiXmlElement("extra_setting");
-		root->LinkEndChild(extra_setting);
-			//Information about extra settings here
+		//Outputter
+		TiXmlElement* outputter = out->to_xml();
+		root->LinkEndChild(outputter);
 		
 		doc.LinkEndChild(decl);
 		doc.LinkEndChild(root);
