9146fbe705fcdb8778ac41fb802aeeb106cb6c8d
Evert Heylen <evertheylen@gmail.com>
2015-12-05 15:40:04 +0100

    meer mac gedoe

--- code/Core/bake_me.py
+++ code/Core/bake_me.py
@@ -16,12 +16,11 @@ if platform.system() != 'Darwin':
         "libs/python3>make>all"
     ]
 
-
-# extra config just for this unit
-# #BakerCanHandleIt
-gcc_config = {
-    "pre_extra": "-Wno-deprecated -Ilibs/python3/Include -Ilibs/boost/b2stuff/include/",
-    "post_extra": "-pthread -Wl,-Bstatic -Llibs/python3 -lpython3.4m -Llibs/boost/b2stuff/lib/ -lboost_python3 -Wl,-Bdynamic -lutil -ldl -lm"
-}
+    # extra config just for this unit
+    # #BakerCanHandleIt
+    gcc_config = {
+        "pre_extra": "-Wno-deprecated -Ilibs/python3/Include -Ilibs/boost/b2stuff/include/",
+        "post_extra": "-pthread -Wl,-Bstatic -Llibs/python3 -lpython3.4m -Llibs/boost/b2stuff/lib/ -lboost_python3 -Wl,-Bdynamic -lutil -ldl -lm"
+    }
 
 executable = "core"
