ca49ef84da2ccb0de4791f496401b1dd152e92fe
Evert Heylen <evertheylen@gmail.com>
2015-12-09 18:32:32 +0100

    Python meer

--- code/NGLib/python_out/bake_me.py
+++ code/NGLib/python_out/bake_me.py
@@ -13,7 +13,7 @@ if platform.system() != 'Darwin':
     # extra config just for this unit
     # #BakerCanHandleIt
     gcc_config = {
-        "pre_extra": "-Wno-deprecated -I/usr/include/python3.5m/ -Ilibs/boost/b2stuff/include/",
-        "post_extra": "-pthread -Wl,-Bdynamic -lutil -ldl -lm -Wl,-Bstatic -Llibs/boost/b2stuff/lib/ -lboost_python3 -Wl,-Bdynamic -lpython3.5m"
+        "pre_extra": "-Wno-deprecated -I/usr/include/%s/ -Ilibs/boost/b2stuff/include/"%project_config["python_lib"],
+        "post_extra": "-pthread -Wl,-Bdynamic -lutil -ldl -lm -Wl,-Bstatic -Llibs/boost/b2stuff/lib/ -lboost_python3 -Wl,-Bdynamic -l%s"%project_config["python_lib"]
     }
 
